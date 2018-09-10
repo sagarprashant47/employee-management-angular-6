4d521a55974caa93a0bf868ad495a48e5dd24f40
Chintan Shah <chintanshah@promactinfo.com>
2018-09-10 17:35:36 +0530

    initial code commit

---
+++ EmployeeManagement/Model/EmployeeManagementContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Model
+{
+    public class EmployeeManagementContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
